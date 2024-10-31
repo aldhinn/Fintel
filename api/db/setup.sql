fa68c27ee0091a4863185274f9043cfc513889c3
aldhinn <aldespinas@gmail.com>
2024-10-31 18:01:00 +0800

    enforcing name uniqueness.

--- api/db/setup.sql
+++ api/db/setup.sql
@@ -6,6 +6,6 @@ CREATE TYPE ASSET_STATUS_TYPE AS ENUM ('active', 'pending');
 /* The table containing information about financial assets. */
 CREATE TABLE IF NOT EXISTS assets (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
     status ASSET_STATUS_TYPE NOT NULL DEFAULT 'pending'
 );
